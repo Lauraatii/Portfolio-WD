cd36d4cf02b706bbcc9b768027b0b1bde98d0a24
Lauraatii <tihanova.laura@gmail.com>
2023-04-21 14:23:28 +0200

    import

--- src/dto/update-ensemble.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEnsembleDto } from './create-ensemble.dto';
-
-export class UpdateEnsembleDto extends PartialType(CreateEnsembleDto) {}
